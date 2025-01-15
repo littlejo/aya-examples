3abe3e1ff570468e029dcc2d3940b759a358891a
Joseph Ligier <littel.jo@gmail.com>
2025-01-15 10:02:50 +0000

    Add test on proto and port

--- loadbalancer/loadbalancer-ebpf/src/main.rs
+++ loadbalancer/loadbalancer-ebpf/src/main.rs
@@ -1,8 +1,18 @@
 #![no_std]
 #![no_main]
 
+mod bindings;
+
 use aya_ebpf::{bindings::xdp_action, macros::xdp, programs::XdpContext};
 use aya_log_ebpf::info;
+
+use bindings::{ethhdr, iphdr, udphdr};
+use core::mem;
+
+const IPPROTO_UDP: u8 = 0x0011;
+const ETH_P_IP: u16 = 0x0800;
+const ETH_HDR_LEN: usize = mem::size_of::<ethhdr>();
+const IP_HDR_LEN: usize = mem::size_of::<iphdr>();
 
 #[xdp]
 pub fn loadbalancer(ctx: XdpContext) -> u32 {
@@ -14,7 +24,43 @@ pub fn loadbalancer(ctx: XdpContext) -> u32 {
 
 fn try_loadbalancer(ctx: XdpContext) -> Result<u32, u32> {
     info!(&ctx, "received a packet");
+    let eth = ptr_at::<ethhdr>(&ctx, 0).ok_or(xdp_action::XDP_PASS)?;
+    if unsafe { u16::from_be((*eth).h_proto) } != ETH_P_IP {
+        return Ok(xdp_action::XDP_PASS);
+    }
+
+    let ip = ptr_at::<iphdr>(&ctx, ETH_HDR_LEN).ok_or(xdp_action::XDP_PASS)?;
+    if unsafe { (*ip).protocol } != IPPROTO_UDP {
+        return Ok(xdp_action::XDP_PASS);
+    }
+
+    let udp = ptr_at_mut::<udphdr>(&ctx, ETH_HDR_LEN + IP_HDR_LEN).ok_or(xdp_action::XDP_PASS)?;
+    let destination_port = unsafe { u16::from_be((*udp).dest) };
+    if destination_port == 9875 {
+        info!(&ctx, "received UDP on port 9875");
+    } else {
+        info!(&ctx, "received a UDP packet");
+    }
+
     Ok(xdp_action::XDP_PASS)
+}
+
+#[inline(always)]
+fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Option<*const T> {
+    let start = ctx.data();
+    let end = ctx.data_end();
+    let len = mem::size_of::<T>();
+
+    if start + offset + len > end {
+        return None;
+    }
+    Some((start + offset) as *const T)
+}
+
+#[inline(always)]
+fn ptr_at_mut<T>(ctx: &XdpContext, offset: usize) -> Option<*mut T> {
+    let ptr = ptr_at::<T>(ctx, offset)?;
+    Some(ptr as *mut T)
 }
 
 #[cfg(not(test))]
