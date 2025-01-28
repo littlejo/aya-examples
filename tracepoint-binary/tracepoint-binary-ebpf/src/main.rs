0e8d13cdbe24c051901e57f6100a7b0ddd6fbe83
Joseph Ligier <littel.jo@gmail.com>
2025-01-28 15:28:42 +0000

    Add tracepoint-binary

---
+++ tracepoint-binary/tracepoint-binary-ebpf/src/main.rs
@@ -0,0 +1,41 @@
+#![no_std]
+#![no_main]
+
+use aya_ebpf::{
+    macros::tracepoint,
+    programs::TracePointContext,
+    helpers::bpf_probe_read_user_str_bytes,
+};
+use aya_log_ebpf::info;
+
+use core::str::from_utf8_unchecked;
+
+const MAX_SMALL_PATH: usize = 16;
+const FILENAME_OFFSET: usize = 16;
+
+#[tracepoint]
+pub fn tracepoint_binary(ctx: TracePointContext) -> u32 {
+    match try_tracepoint_binary(ctx) {
+        Ok(ret) => ret,
+        Err(ret) => ret as u32,
+    }
+}
+
+fn try_tracepoint_binary(ctx: TracePointContext) -> Result<u32, i64> {
+    let mut buf = [0u8; MAX_SMALL_PATH];
+
+    let filename = unsafe {
+        let filename_src_addr = ctx.read_at::<*const u8>(FILENAME_OFFSET)?;
+        let filename_bytes = bpf_probe_read_user_str_bytes(filename_src_addr, &mut buf)?;
+        from_utf8_unchecked(filename_bytes)
+    };
+
+    info!(&ctx, "tracepoint sys_enter_execve called. Binary: {}", filename);
+    Ok(0)
+}
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
