8954d5e57207cf430317798b424773c4a35b541d
Joseph Ligier <littel.jo@gmail.com>
2025-02-28 08:36:16 +0000

    Change PerCpuHashMap into  PerCpuArray

--- tracepoint-binary-maps/tracepoint-binary-ebpf/src/main.rs
+++ tracepoint-binary-maps/tracepoint-binary-ebpf/src/main.rs
@@ -5,7 +5,7 @@ use aya_ebpf::{
     macros::{tracepoint, map},
     programs::TracePointContext,
     helpers::bpf_probe_read_user_str_bytes,
-    maps::{PerCpuHashMap, HashMap},
+    maps::{PerCpuArray, HashMap},
 };
 
 use tracepoint_binary_common::MAX_PATH_LEN;
@@ -15,11 +15,10 @@ use aya_log_ebpf::info;
 use core::str::from_utf8_unchecked;
 
 const FILENAME_OFFSET: usize = 16;
-const DEFAULT_KEY: u8 = 0;
 const ZEROED_ARRAY: [u8; MAX_PATH_LEN] = [0u8; MAX_PATH_LEN];
 
 #[map]
-static BUF: PerCpuHashMap<u8, [u8; MAX_PATH_LEN]> = PerCpuHashMap::with_max_entries(1, 0);
+static BUF: PerCpuArray<[u8; MAX_PATH_LEN]> = PerCpuArray::with_max_entries(1, 0);
 
 #[map]
 static EXCLUDED_CMDS: HashMap<[u8; 512], u8> = HashMap::with_max_entries(10, 0);
@@ -33,10 +32,10 @@ pub fn tracepoint_binary(ctx: TracePointContext) -> u32 {
 }
 
 fn try_tracepoint_binary(ctx: TracePointContext) -> Result<u32, i64> {
-    BUF.insert(&DEFAULT_KEY, &ZEROED_ARRAY, 0)?;
-    let buf = BUF.get_ptr_mut(&DEFAULT_KEY).ok_or(0)?;
+    let buf = BUF.get_ptr_mut(0).ok_or(0)?;
 
     let filename = unsafe {
+        *buf = ZEROED_ARRAY;
         let filename_src_addr = ctx.read_at::<*const u8>(FILENAME_OFFSET)?;
         let filename_bytes = bpf_probe_read_user_str_bytes(filename_src_addr, &mut *buf)?;
         if EXCLUDED_CMDS.get(&mut *buf).is_some() {
